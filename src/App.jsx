52bf8125090fd690f4aceb78b7deca8c4b9933cd
Sahinhub <sahinhub@gmail.com>
2025-09-06 15:05:49 +0600

    service section is finished

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import { Outlet } from 'react-router';
 import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
    <div className=''>
+    <Analytics/>
     <Header></Header>
     <Outlet></Outlet>
     <Footer></Footer>
