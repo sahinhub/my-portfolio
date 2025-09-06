1e0862803c00204b55627f36c55df64a2354a5ce
Sahinhub <sahinhub@gmail.com>
2025-09-06 15:04:05 +0600

    service section is finished

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router';
 import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header'
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   return (
